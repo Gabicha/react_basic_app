316e2f776dad3b759d13c4863d8b088a7aba4d94
Cristian Pinto <cdpinto@me.com>
2015-08-17 08:38:42 -0500

    Adds 'active' toggle to top menu

--- js/components/menu.js
+++ js/components/menu.js
@@ -1,15 +1,27 @@
 'use strict';
 
 var Menu = React.createClass({
+  selectMenu: function(event) {
+    this.setState({activeMenu: event.target.id});
+  },
+  getInitialState: function() {
+    return {
+      activeMenu: 'home'
+    };
+  },
   render: function() {
     return (
       <div className="blog-masthead">
         <div className="container">
           <nav className="blog-nav">
-            <a className="blog-nav-item active" href="#">Inicio</a>
-            <a className="blog-nav-item" href="#">Todos los posts</a>
-            <a className="blog-nav-item" href="#">Como postear?</a>
-            <a className="blog-nav-item" href="#">Acerca del sitio</a>
+            <a id="home" className={this.state.activeMenu == 'home' ? "active blog-nav-item" : "blog-nav-item"}
+               href="#" onClick={this.selectMenu}>
+              Inicio
+            </a>
+            <a id="about" className={this.state.activeMenu == 'about' ? "active blog-nav-item" : "blog-nav-item"}
+               href="#" onClick={this.selectMenu}>
+              Acerca del sito
+            </a>
           </nav>
         </div>
       </div>
