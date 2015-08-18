0a8ac5608fd3e3f2a071240b2a5a89e4d48190bc
diablouma <mh732@hotmail.com>
2015-08-18 09:53:38 -0500

    @diablouma Overrides bootrap bower.json to point to css file instead of less file

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,18 @@ var debug = require('gulp-debug');
 var server = require('gulp-server-livereload');
 var react = require('gulp-react');
 
-
 gulp.task('index', function () {
-    var bowerFiles = mainBowerFiles();
+    var bowerFiles = mainBowerFiles({
+        "overrides": {
+          "bootstrap": {
+            "main": [
+              "dist/css/bootstrap.css",
+              "dist/js/bootstrap.js"
+            ],
+          }
+        }
+    });
+
     return gulp.src('./index.html')
         .pipe(inject(gulp.src(bowerFiles, {read: false}), {name: 'bower', relative: true}))
         .pipe(gulp.dest('./'));
