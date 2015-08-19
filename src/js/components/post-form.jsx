28e11c5f0ceb77658b3d86a37964908c4defd112
diablouma <mh732@hotmail.com>
2015-08-19 12:12:53 -0500

    @diablouma Images now are saved to the server when they are uploaded

--- src/js/components/post-form.jsx
+++ src/js/components/post-form.jsx
@@ -31,4 +31,41 @@ React.render(
   document.getElementById('new-post')
 );
 
-AlloyEditor.editable('post-content-editable');
+var uploadImageToServer = function(alloyEditor) {
+  var endpointToUploadImages = "http://localhost:9292/api/images/";
+  var mediaEndpoint = "http://localhost:9292/media/";
+
+
+  alloyEditor.get('nativeEditor').on('imageAdd', function(event) {
+    var imgElement = getImgElement(event);
+
+    $.ajax({
+      url: endpointToUploadImages,
+      dataType: 'json',
+      type: 'POST',
+      data: {'encoded_image': getEncodedImageFromSrc(imgElement),
+             'image_type': getImageTypeFromSrc(imgElement)},
+      success: function(createdFile) {
+        imgElement['$'].src = mediaEndpoint + createdFile.name
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(endpointToUploadImages, status, err.toString());
+      }.bind(this)
+    });
+  });
+};
+
+var getImgElement = function(event) {
+  return event.data.el;
+};
+
+var getEncodedImageFromSrc = function(imgElement) {
+    return imgElement['$']['src'].split(',')[1];
+};
+
+var getImageTypeFromSrc = function(imgElement) {
+  return imgElement['$']['src'].split(',')[0].split(/[:/;]+/)[2];
+};
+
+var alloyEditor = AlloyEditor.editable('post-content-editable');
+uploadImageToServer(alloyEditor);
